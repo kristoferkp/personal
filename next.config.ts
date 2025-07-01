80fe650c3bc7cfb9453c27796844fdb9e906e744
Kristofer <kristofer.polluste@gmail.com>
2025-07-01 22:37:14 +0300

    setup github actions

--- next.config.ts
+++ next.config.ts
@@ -7,5 +7,7 @@ const withNextra = nextra({
 
 // Export the final Next.js config with Nextra included
 export default withNextra({
-	// ... Add regular Next.js options here
+	output: "export",  // <=== enables static exports
+  	reactStrictMode: true,
+	basePath: "/personal",  // <=== sets the base path for the app
 });
